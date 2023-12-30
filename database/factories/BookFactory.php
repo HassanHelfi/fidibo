a8d5a5732b99beeca622c164245bb918f1922647
HassanHelfi <hbhassanhelfi@gmail.com>
2023-12-31 00:35:02 +0330

    feat: add book table, model and faktory

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(4,true),
+            'cover' => fake()->image(),
+            'type' => fake()->numberBetween(1, 2),
+        ];
+    }
+}
