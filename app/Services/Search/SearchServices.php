225832a1fc141066abaf3c52ed54916cca9e9b06
HassanHelfi <hbhassanhelfi@gmail.com>
2023-12-31 22:30:08 +0330

    feat: add time to live for search servise

--- app/Services/Search/SearchServices.php
+++ app/Services/Search/SearchServices.php
@@ -13,7 +13,9 @@ class SearchServices
 
     public function search(string $keyword): Collection
     {
-        return Cache::remember('search', 10, function() use($keyword){
+        $ttl = config('cache.search.ttl', 60 * 60 * 12);
+
+        return Cache::remember('search.' . $keyword,  $ttl, function () use ($keyword) {
             return $this->searchRepository->search($keyword);
         });
     }
