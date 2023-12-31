5030d818b77d783948406d44eed85d3837451f1e
HassanHelfi <hbhassanhelfi@gmail.com>
2023-12-31 22:25:55 +0330

    feat: add get type attribute for book type

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Enums\BookTypeEnum;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,4 +14,14 @@ class Book extends Model
     protected $fillable = [
         'title', 'cover', 'type'
     ];
+
+    protected function type(): Attribute
+    {
+        return Attribute::make(
+            get: fn (int $value) => match($value) {
+                BookTypeEnum::AUDIOBOOK->value => BookTypeEnum::AUDIOBOOK->name,
+                BookTypeEnum::TEXTBOOK->value => BookTypeEnum::TEXTBOOK->name,
+            }
+        );
+    }
 }
