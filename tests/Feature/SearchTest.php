9608441aff7db96816f04de42344f318f590833b
HassanHelfi <hbhassanhelfi@gmail.com>
2023-12-31 23:57:21 +0330

    test: write test for search

---
+++ tests/Feature/SearchTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Book;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+
+class SearchTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $book = Book::factory()->create([
+            'title' => 'Test Book',
+            'cover' => 'test-cover.jpg',
+            'type' => 1,
+        ]);
+
+        Cache::shouldReceive('remember')
+            ->once()
+            ->withAnyArgs()
+            ->andReturn(collect([$book]));
+
+        $response = $this->get(route('search', ['search' => 'Test']));
+
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                '*' => [
+                    'title',
+                    'cover',
+                    'type',
+                ]
+            ]);
+    }
+}
